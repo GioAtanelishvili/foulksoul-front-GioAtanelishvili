1bca210adf8824df9d95bfbf678484018bb4b480
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-08-03 08:20:17 +0400

    style: update member biography textarea styles

--- src/components/MemberTextArea.tsx
+++ src/components/MemberTextArea.tsx
@@ -2,7 +2,11 @@ import { TextAreaProps } from 'types';
 
 const MemberTextArea: React.FC<TextAreaProps> = (props) => {
   return (
-    <div className='w-1/2 h-56 mt-11 mb-1 py-3 pl-2 pr-3 relative text-sm border border-primary-dark-blue rounded-[5px]'>
+    <div
+      className={`w-1/2 h-56 mt-11 mb-1 py-3 pl-2 pr-3 relative text-sm border rounded-[5px] ${
+        props.error ? 'border-input-error' : 'border-primary-dark-blue'
+      }`}
+    >
       <textarea
         id='members-update-textarea'
         name={props.register.name}
@@ -13,7 +17,7 @@ const MemberTextArea: React.FC<TextAreaProps> = (props) => {
         className='w-full min-h-full resize-none rounded-[5px] outline-none text-primary-dark-blue placeholder:text-update-form-placeholder'
       />
       {props.error && (
-        <p className='text-login-input-error w-full text-center absolute -bottom-6 font-medium'>
+        <p className='text-input-error w-full text-center absolute -bottom-6 font-medium'>
           {props.error.message}
         </p>
       )}
